78fc49dbcaf6c1bdd1826128af085ca53e507f55
Naoto Mukai <nmukai@sugiyama-u.ac.jp>
2022-10-20 21:00:32 +0900

    pwa

---
+++ application/WeatherPainApp/sw.js
@@ -0,0 +1,27 @@
+// Cache name
+const CACHE_NAME = 'wpa-caches-v1';
+
+// Cache targets
+const urlsToCache = [
+  './',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => {
+        return response ? response : fetch(event.request);
+      })
+  );
+});
