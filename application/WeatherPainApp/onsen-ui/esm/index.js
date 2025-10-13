795b23b6ef59878e8151dc5648d71ed2f5c7e0bc
Naoto Mukai <nmukai@sugiyama-u.ac.jp>
2025-10-13 21:46:15 +0900

    mikke-ar

--- application/WeatherPainApp/onsen-ui/esm/index.js
+++
@@ -1,6 +0,0 @@
-import setup from './setup.js'; // Add polyfills
-import ons from './ons/index.js'; // External dependency, always hoisted
-
-setup(ons); // Setup initial listeners
-
-export default ons;
--- application/WeatherPainApp/register.js
+++
@@ -1,7 +0,0 @@
-if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('./sw.js').then(registration => {
-	console.log('ServiceWorker registration successful.');
-    }).catch(err => {
-	console.log('ServiceWorker registration failed.');
-    });
-}    
